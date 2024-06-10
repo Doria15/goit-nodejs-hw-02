cd78aa1e60279449fd1c8a673f016bf96776dd9b
Doria15 <doriaceelin@gmail.com>
2024-06-10 16:41:58 +0800

    Updated codes

--- routes/api/usersRouter.js
+++ routes/api/usersRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { controlWrapper } from "../../helpers/controlWrapper.js";
 // prettier-ignore
-import { signupUser, loginUser, logoutUser, getCurrentUsers, updateUserSubscription} from "../../controllers/usersController.js";
+import { signupUser, loginUser, logoutUser, getCurrentUsers, updateUserSubscription, updateAvatar} from "../../controllers/usersController.js";
 import { authenticateToken } from "../../middlewares/authenticateToken.js";
+import { upload } from "../../middlewares/upload.js";
 
 const router = express.Router();
 
@@ -15,4 +16,8 @@ router.get("/logout", authenticateToken, controlWrapper(logoutUser));
 router.get("/current", authenticateToken, controlWrapper(getCurrentUsers));
 
 router.patch("/", authenticateToken, controlWrapper(updateUserSubscription));
+
+// prettier-ignore
+router.patch("/avatars", authenticateToken, upload.single("avatar"), controlWrapper(updateAvatar));
+
 export { router };
