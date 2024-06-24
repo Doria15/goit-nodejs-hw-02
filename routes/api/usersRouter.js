d0330f4902e6f1321d3416079fadd7d316eb412c
Doria15 <doriaceelin@gmail.com>
2024-06-24 15:28:48 +0800

    Updated Codes

--- routes/api/usersRouter.js
+++ routes/api/usersRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { controlWrapper } from "../../helpers/controlWrapper.js";
 // prettier-ignore
-import { signupUser, loginUser, logoutUser, getCurrentUsers, updateUserSubscription, updateAvatar} from "../../controllers/usersController.js";
+import {signupUser, loginUser, logoutUser, getCurrentUsers, updateUserSubscription, updateAvatar, verifyEmail, resendVerifyEmail} from "../../controllers/usersController.js";
 import { authenticateToken } from "../../middlewares/authenticateToken.js";
 import { upload } from "../../middlewares/upload.js";
 
@@ -19,5 +19,9 @@ router.patch("/", authenticateToken, controlWrapper(updateUserSubscription));
 
 // prettier-ignore
 router.patch("/avatars", authenticateToken, upload.single("avatar"), controlWrapper(updateAvatar));
+
+router.get("/verify/:verificationToken", controlWrapper(verifyEmail));
+
+router.post("/verify", authenticateToken, controlWrapper(resendVerifyEmail));
 
 export { router };
